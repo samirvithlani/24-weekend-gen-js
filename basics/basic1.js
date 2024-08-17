fcfbd575bd77641a863055f1a9e213384d9e0fd6
samirvithlani <samir.vithlani83955@gmail.com>
2024-08-17 18:51:46 +0530

    basics

---
+++ basics/basic1.js
@@ -0,0 +1,28 @@
+//printf()
+console.log("Hello World");
+
+var a = 100 //int //number
+//printf("%d",a);
+//printf("value of a = %d",a);//
+console.log(a)
+console.log("value of a ::",a)
+//type of operator
+console.log("type of a = ",typeof a)
+
+var pers = 90.87
+console.log("value of pers ::",pers)
+console.log("type of pers = ",typeof pers)
+
+var sname = "raj" //string
+console.log("value of name ::",sname)
+console.log("type of name = ",typeof sname)
+
+
+var isPass = true //boolean
+console.log("value of isPass ::",isPass)
+console.log("type of isPass = ",typeof isPass)
+
+
+var x; //undefined
+console.log("value of x ::",x)
+console.log("type of x = ",typeof x)

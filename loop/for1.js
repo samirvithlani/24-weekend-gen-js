bc1416d479ffecb3b5fe35c93e967868202e0c8b
samirvithlani <samir.vithlani83955@gmail.com>
2024-08-31 18:30:56 +0530

    loop

---
+++ loop/for1.js
@@ -0,0 +1,12 @@
+for(let i=1;i<=10;i++){
+
+    console.log(i);
+}
+
+
+//var sp = 1
+//var ep = 20
+
+//print even bw start and end
+//print sum of all numbers bw start and end
+//print sum of odd numbers bw start and end
---
+++ switch/switchDemo1.js
@@ -0,0 +1,52 @@
+/*
+if
+else if
+else if
+else
+
+*/ 
+
+/*
+    onluy a single block of code will be executed
+    if(choice==1)
+    if(choice==2)
+
+*/ 
+
+var choice = 22
+
+switch(choice){
+
+    case 1:
+        console.log("Case 1");
+        break;
+    case 2:
+        console.log("Case 2");
+        break;  
+    default:
+        console.log("Default");
+        break;      
+}
+
+
+
+var season = "summer";
+switch(season){
+
+
+        case "summer":
+            console.log("its summer");
+            break;
+        case "winter":
+            console.log("its winter");
+            break;
+        case "spring":
+            console.log("its spring");
+            break;
+        case "autumn":
+            console.log("its autumn");
+            break;    
+
+
+
+}

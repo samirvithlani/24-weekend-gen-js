365256f9713102d96d79f1494c565db91278e5dc
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-07 18:20:10 +0530

    prompt

---
+++ prompt/promptdemo.js
@@ -0,0 +1,26 @@
+//scanf("%d",&no)
+//var no = prompt("Enter a number: ");
+//"100" parseInt '"ancd" NaN
+// var no = parseInt(prompt("Enter a number: "));
+// console.log("You entered: " , no);
+// console.log("Type of no: " , typeof no);
+
+
+// var name = prompt("Enter your name: ");
+// console.log("You entered: " , name);
+
+
+// var no1 = parseInt(prompt("Enter first number: "));
+// var no2 = parseInt(prompt("Enter second number: "));
+
+// var ans = no1 + no2;
+//alert("Sum of "+ no1 +" and "+ no2 +" is: "+ ans);
+//alert("ans = "+ans);
+
+var age = parseInt(prompt("Enter your age:\n please enter digits only"));
+if(age>=18){
+    alert("You are eligible to vote");
+}
+else{
+    alert("You are not eligible to vote");
+}

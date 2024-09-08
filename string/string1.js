73331b3e1238bbe72f5257e1b9af34271d807439
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-08 14:26:27 +0530

    string

---
+++ string/string1.js
@@ -0,0 +1,23 @@
+var x = "royal"
+console.log(x)
+// console.log(x[0])//
+// console.log(x[1])//
+// console.log(x[2])//
+// console.log(x[3])//
+// console.log(x[4])//
+//console.log(x[5])//
+
+for(let i=0;i<5;i++){
+    console.log(x[i])
+}
+
+//concatenation
+
+var data = "royal"
+//data + " technosoft"//immutable
+var p = data + " technosoft"
+//data = data + " technosoft" new reference
+console.log(data)
+console.log(p)
+
+//find no of spaces in a string

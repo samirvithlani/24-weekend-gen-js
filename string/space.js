ad73da716db4b8d734892ce40cb1b480bf3cbe3c
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-08 14:50:33 +0530

    index find

---
+++ string/space.js
@@ -0,0 +1,19 @@
+var sent = "India is my country and all Indians are my brothers and sisters";
+
+// var len = sent.length;
+// console.log(len);
+var count =0;
+for(let i=0;i<sent.length;i++){
+
+    //India is my
+    if(sent[i]==" "){
+        count++;
+    }
+}
+
+console.log("no if space in str = ",count+1);
+
+
+
+var data = "java java is bike";
+var c = "a" // 1

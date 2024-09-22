93394c93aa374bcc6645fdfcafcd7a7a80d95ec1
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-22 14:26:43 +0530

    array

---
+++ array/array4.js
@@ -0,0 +1,18 @@
+var lang = ["gujarati","hindi","english","marathi","tamil"];
+
+console.log(lang)
+lang.push("telugu"); // add element at the end of array
+console.log(lang)
+var removedElm =  lang.pop(); // remove element from the end of array
+console.log("removing...",removedElm)
+console.log(lang)
+
+
+
+//startin of array
+
+lang.unshift("Sanscrit"); // add element at the start of array
+console.log(lang)
+removedElm = lang.shift(); // remove element from the start of array'
+console.log("removing...",removedElm)
+console.log(lang)
---
+++ array/array5.js
@@ -0,0 +1,8 @@
+var lang = ["gujarati","hindi","english","marathi","tamil"];
+console.log(lang)
+//
+//lang.splice(0,2) //0is the index , 2 is the number of elements to be removed
+//lang.splice(2,1) //2is the index , 1 is the number of elements to be removed
+//lang.splice(2,0,"kannada","french") //2is the index , 1 is the number of elements to be removed
+lang.splice(3,0,"abc")
+console.log(lang)

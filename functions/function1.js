048b53596b1e73df0fbcac83fc355bf36cfb61fc
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-28 18:30:16 +0530

    function

---
+++ functions/function1.js
@@ -0,0 +1,28 @@
+function add(){
+    //without parameters and without return type
+    let a = 100
+    let b = 200
+    let c = a + b
+    console.log(c)
+}
+
+
+function sub(){
+    //without parameters and with return type
+    let a = 100
+    let b = 200
+    let c = a - b
+    console.log(c)
+}
+
+function drawLine(){
+
+    console.log("____________________________________________________________")
+}
+
+drawLine()//function call
+add()//function call
+drawLine()//function call
+sub()//function call
+drawLine()//function call
+

2d59b235bf2679aef96e7ac6af7d446f41719fcd
samirvithlani <samir.vithlani83955@gmail.com>
2024-10-05 18:32:27 +0530

    ternery op

---
+++ operator/ternery/ternop.js
@@ -0,0 +1,45 @@
+var age =18;
+// if(age>=18){
+//     console.log("You are eligible to vote");
+// }
+// else{
+//     console.log("You are not eligible to vote");
+// }
+
+age>=18 ? console.log("valid") : console.log("invalid");
+
+
+var num =23;
+num %2 ==0 ? console.log("Even") : console.log("Odd");
+// if(num %2 == 0){
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd");
+// }
+
+var no1 = 10
+var no2 = 20
+//var max;
+
+// if(no1>no2){
+//     max = no1;
+// }
+// //    console.log("No1 is greater");
+
+// else{
+//     max = no2;
+//   //  console.log("No2 is greater");
+// }
+// console.log("max = ",max);
+
+
+//no1>no2 ? console.log("No1 is greater") : console.log("No2 is greater");
+
+var max = no1> no2 ? no1 : no2;
+console.log("max = ",max);
+
+
+
+
+

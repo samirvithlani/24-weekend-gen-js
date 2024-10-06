ede04638e0c746d1bb4fbcecda52dcfbd70a2f4d
samirvithlani <samir.vithlani83955@gmail.com>
2024-10-06 13:27:56 +0530

    forEach

---
+++ array/array6.js
@@ -0,0 +1,21 @@
+var lang = ["Gujarati", "Hindi", "English", "Marathi"];
+
+// for(let i=0;i<lang.length;i++){
+//     console.log(lang[i]);
+// }
+
+//forEach..
+
+// lang.forEach(function(l){
+//     console.log(l);
+// })
+
+// l == lang[i]
+lang.forEach((l)=>{
+    console.log(l);
+})
+var marks = [12,22,34,56,78]
+
+marks.forEach((mark)=>{
+    console.log(mark);
+})

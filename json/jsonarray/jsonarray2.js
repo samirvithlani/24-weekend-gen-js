c5076474c52926d87f01737bfd500459d56ec58a
samirvithlani <samir.vithlani83955@gmail.com>
2024-11-17 14:33:47 +0530

    json

---
+++ json/jsonarray/jsonarray2.js
@@ -0,0 +1,76 @@
+var cars =[
+    {
+        id:1001,
+        compnay:"Maruti",
+        model:"Swift",
+        price:700000,
+        maxSpeed:180,
+        fuelType:"Petrol",
+        colors:["red","blue","white"],
+        engineType:"K12M",
+        features:["power steering","power windows","music system"],
+        safetyFeatures:["ABS","EBD","Airbags"],
+        interiorFeatures:{
+            seats:5,
+            material:"fabric",
+            musicSystem:"touchscreen"
+        },
+        exteriorFeatures:{
+            headLamps:"LED",
+            tailLamps:"LED",
+            mirrors:"electric"
+        }
+    },
+    {
+        id:1002,
+        compnay:"Hyundai",
+        model:"i20",
+        price:800000,
+        maxSpeed:190,
+        fuelType:"Petrol",
+        colors:["red","blue","white","black"],
+        engineType:"Kappa",
+        features:["power steering","power windows","music system","sunroof"],
+        safetyFeatures:["ABS","EBD","Airbags"],
+        interiorFeatures:{
+            seats:5,
+            material:"leather",
+            musicSystem:"touchscreen"
+        },
+        exteriorFeatures:{
+            headLamps:"LED",
+            tailLamps:"LED",
+            mirrors:"electric"
+        }
+    },
+    {
+        id:1003,
+        compnay:"Tata",
+        model:"Nexon",
+        price:900000,
+        maxSpeed:200,
+        fuelType:"Diesel",
+        colors:["red","blue","white","black","grey"],
+        engineType:"Revotorq",
+        features:["power steering","power windows","music system","sunroof","climate control"],
+        safetyFeatures:["ABS","EBD","Airbags","ESP"],
+        interiorFeatures:{
+            seats:5,
+            material:"leather",
+            musicSystem:"touchscreen"
+        },
+        exteriorFeatures:{
+            headLamps:"LED",
+            tailLamps:"LED",
+            mirrors:"electric"
+        }
+    }
+]
+
+//print all car names,
+//print all car names with price less than 800000,
+//print all car with fuel type petrol,
+//print all car with fuel type petrol and price less than 800000,
+//print all cars wich has blue color,
+//print all cars with capacity 5,
+//print all cars with music system,

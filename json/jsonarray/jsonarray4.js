7ffa8c30cacb75c11738cbb2cd3a862a2b97491e
samirvithlani <samir.vithlani83955@gmail.com>
2024-11-30 18:18:46 +0530

    task

---
+++ json/jsonarray/jsonarray4.js
@@ -0,0 +1,74 @@
+const contries = [
+    {
+        name:"India",
+        capital:"New Delhi",
+        states:[
+            {
+                name:"Guajarat",
+                population:70000000,
+                area:20000,
+                aqi:200,
+
+            },
+            {
+                name:"Maharashtra",
+                population:100000000,
+                area:30000,
+                aqi:300
+            },
+            {
+                name:"Rajasthan",
+                population:80000000,
+                area:40000,
+                aqi:250
+            }
+        ]
+    },
+    {
+        name:"USA",
+        capital:"Washington DC",
+        states:[
+            {
+                name:"California",
+                population:40000000,
+                area:50000,
+                aqi:150
+            },
+            {
+                name:"Texas",
+                population:30000000,
+                area:60000,
+                aqi:100
+            },
+            {
+                name:"Florida",
+                population:20000000,
+                area:70000,
+                aqi:50
+            }
+        ]
+    },
+    {
+        name:"Australia",
+        capital:"Canberra",
+        states:[
+            {
+                name:"New South Wales",
+                population:15000000,
+                area:80000,
+                aqi:70
+            },
+            {
+                name:"Victoria",
+                population:12000000,
+                area:90000,
+                aqi:90
+            }
+        ]
+    }
+]
+//find all states of India
+//find all satte of usa which has population more than 25000000
+//find all states of australia which has area less than 80000
+//find all states of india which has aqi less than 250
+//find all states of usa which has aqi less than 100

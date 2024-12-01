bbc420b98c83d8105639f8b2bba2a69d311f3ef1
samirvithlani <samir.vithlani83955@gmail.com>
2024-12-01 14:28:24 +0530

    question

---
+++ json/jsonarray/jsonarray5.js
@@ -0,0 +1,58 @@
+const questions = [
+    {
+        question:"Where is the capital of India?",
+        options:["New Delhi","Mumbai","Kolkata","Chennai"],
+        answer:0,
+        topic:"India",
+        subject:"General Knowledge"
+    },
+    {
+        question:"Which state has the highest population in India?",
+        options:["Maharashtra","New Delhi","Bihar","West Bengal"],
+        answer:1,
+        topic:"India",
+        subject:"General Knowledge"
+    },
+    {
+        question:"Which state has the highest area in India?",
+        options:["Rajasthan","Madhya Pradesh","Maharashtra","Uttar Pradesh"],
+        answer:0,
+        topic:"India",
+        subject:"General Knowledge"
+    },
+    {
+        question:"Which state has the highest AQI in India?",
+        options:["New Delhi","Uttar Pradesh","Bihar","West Bengal"],
+        answer:1,
+        topic:"India",
+        subject:"General Knowledge"
+    },
+    {
+        question:"what is 1+1?",
+        options:["1","2","3","4"],
+        answer:1,
+        topic:"Math",
+        subject:"Arithmetic"
+    },
+    {
+        question:"what is highest number?",
+        options:["1","2","3","4"],
+        answer:3,
+        topic:"Math",
+        subject:"Arithmetic"
+    },
+    {
+        question:"formula of water?",
+        options:["H2O","CO2","O2","H2"],
+        answer:0,
+        topic:"Science",
+        subject:"Chemistry"
+    }
+    
+   
+]
+
+//find all questions of India
+//find all questions of Math
+//find all questions which has answer is New Delhi
+//find how many questions have answer is 1
